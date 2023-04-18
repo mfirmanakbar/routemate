ba553f8e3fa6691ed49bee64779d1637c1b759f6
Firman <109858426+firmangits@users.noreply.github.com>
2023-04-18 15:27:39 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./components/Home";
+import { ProductList } from "./components/ProductList";
+import { ProductDetail } from "./components/ProductDetail";
+import { Contact } from "./components/Contact";
+
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className="App">
+      <header>HEADER</header>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="products" element={<ProductList />}></Route>
+        <Route path="products/1001" element={<ProductDetail />}></Route>
+        <Route path="contact" element={<Contact />}></Route>
+      </Routes>
+      <footer>FOOTER</footer>
     </div>
   );
 }
