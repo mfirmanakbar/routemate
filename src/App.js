abe3fe550c819c4473b1dac842265b07c642d8a4
Firman <109858426+firmangits@users.noreply.github.com>
2023-04-18 16:39:06 +0700

    update navlink link

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Routes, Route } from "react-router-dom";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 import { Home } from "./components/Home";
 import { ProductList } from "./components/ProductList";
 import { ProductDetail } from "./components/ProductDetail";
@@ -9,14 +11,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header>HEADER</header>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="products" element={<ProductList />}></Route>
-        <Route path="products/1001" element={<ProductDetail />}></Route>
-        <Route path="contact" element={<Contact />}></Route>
-      </Routes>
-      <footer>FOOTER</footer>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="products" element={<ProductList />} />
+          <Route path="products/1001" element={<ProductDetail />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,9 @@
+import { Link } from "react-router-dom";
+
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <Link to="/">2030 Routemate</Link>
+    </footer>
   )
 }
