1b6d49e06af422bd36558046cc13952f55f8b80e
mfirmanakbar <em.firman.akbar@gmail.com>
2023-04-20 09:21:28 +0700

    adding admin component and update contact. navigate and useNavigate

---
+++ src/components/Admin.js
@@ -0,0 +1,5 @@
+export const Admin = () => {
+  return (
+    <div className="component">Welcom to Admin Section</div>
+  )
+}
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 export const Contact = () => {
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    console.log("--------");
+    return navigate("/");
+  }
+
   return (
-    <div>Contact</div>
+    <>
+    <div className="component">Contact</div>
+    <button onClick={handleSubmit}>Submit Form</button>
+    </>
   )
 }
