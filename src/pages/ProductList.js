7cf381c42dc0e5754e680373d7b5fe786e83c704
mfirmanakbar <em.firman.akbar@gmail.com>
2023-04-20 22:25:18 +0700

    update

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -4,11 +4,11 @@ export const ProductList = () => {
   const [searchParams] = useSearchParams();
   const location = useLocation();
 
-  // console.log(searchParams.get("keyword"));
-  // console.log(searchParams.get("instock"));
-  // console.log(searchParams.get("rating"));
+  console.log(searchParams.get("keyword"));
+  console.log(searchParams.get("instock"));
+  console.log(searchParams.get("rating"));
 
-  //console.log(location);
+  console.log(location);
 
   return (
     <main>
